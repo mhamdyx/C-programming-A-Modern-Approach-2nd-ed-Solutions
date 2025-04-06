bac24fe1ae09a41ed89996c4f784886ef6dadc80
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-04-06 18:14:22 +0200

    make ch2 pp7 more relevant

--- Ch02_C_Fundamentals/ch2_prog_proj_07.c
+++ Ch02_C_Fundamentals/ch2_prog_proj_07.c
@@ -11,24 +11,26 @@
 
 int main(void)
 {
-	int usd_amount, bills;
+	int usd_amount, remainder, bills;
 
 	printf("Enter a dollar amount: ");
 	scanf("%d", &usd_amount);
 
 	bills = usd_amount / 20;
 	printf("$20 bills: %d\n", bills);
-	usd_amount %= 20;
+	remainder = usd_amount - bills * 20;
 
+	usd_amount = remainder;
 	bills = usd_amount / 10;
 	printf("$10 bills: %d\n", bills);
-	usd_amount %= 10;
+	remainder = usd_amount - bills * 10;
 
+	usd_amount = remainder;
 	bills = usd_amount / 5;
 	printf(" $5 bills: %d\n", bills);
-	usd_amount %= 5;
+	remainder = usd_amount - bills * 5;
 
-	printf(" $1 bills: %d\n", usd_amount);
+	printf(" $1 bills: %d\n", remainder);
 
 	return 0;
 }
