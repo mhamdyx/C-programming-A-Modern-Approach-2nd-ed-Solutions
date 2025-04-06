ec24201bc0068348ca9ae597b8161a5a63936c78
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-04-06 17:41:09 +0200

    add ch2 prog proj 1

---
+++ Ch02_C_Fundamentals/ch2_prog_proj_01.c
@@ -0,0 +1,33 @@
+/*
+ * ch2_prog_proj_01.c
+ *
+ *  Created on: Apr 6, 2025
+ *      Author: Mahmoud Hamdy
+ */
+
+// Project 1: Picture on the screen
+
+/*
+ * x [] []|[] [] [] [] [] [] [] x
+ *   x  []|[] [] [] [] [] [] x
+ *      x |[] [] [] [] [] x
+ *        |x  [] [] [] x
+ *        |   x  [] x
+ *        |      x
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    
+    return 0;
+}
+ 
