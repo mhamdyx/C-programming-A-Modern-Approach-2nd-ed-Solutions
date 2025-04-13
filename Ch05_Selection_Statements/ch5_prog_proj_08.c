38164740b3e86142eead05a82448e99e4b4b2cb9
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-04-13 16:34:05 +0200

    update ch5 pp8 solution

--- Ch05_Selection_Statements/ch5_prog_proj_08.c
+++ Ch05_Selection_Statements/ch5_prog_proj_08.c
@@ -11,156 +11,61 @@
 
 int main(void)
 {
-	int hh_desired, mm_desired, desired_time;
-	int hh_dep, mm_dep, closest_dep_time;
-	int hh_arrive, mm_arrive, arrive_time;
-	int curr_time_diff; // Computing the current time difference for each departure time
-	int min_time_diff = 1440; // Assuming the start value is 24 hr diff (max diff)
-	char dep_meridiem, arrive_meridiem; // a.m or p.m?
+	int hr_24form, mins, total_desired_time;
 
-	// Reading user input (desired time)
+	// 8:00 AM, 9:43 AM
+	int dep_time1 = 480, dep_time2 = 583;
+
+	// 11:19 AM, 12:47 PM
+	int dep_time3 = 679, dep_time4 = 767;
+
+	// 2:00 PM, 3:45 PM
+	int dep_time5 = 840, dep_time6 = 945;
+
+	// 7:00 PM, 9:45 PM
+	int dep_time7 = 1140, dep_time8 = 1305;
+
 	printf("Enter a 24-hour time: ");
-	scanf("%d:%d", &hh_desired, &mm_desired);
+	scanf("%d :%d", &hr_24form, &mins);
 
-	// Convert the desired time from hh:mm format to total minutes format
-	desired_time = hh_desired * 60 + mm_desired;
+	total_desired_time = hr_24form * 60 + mins;
 
-	// Finding the closest time to our desired time according to the given table
-	// 08:00 a.m.
-	curr_time_diff = 480 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
+	printf("Closest departure time is ");
 
-	if(min_time_diff > curr_time_diff)
+	// Finding the closest departure time between each pair
+	if (total_desired_time <= (dep_time1 + (dep_time2 - dep_time1) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 480; // 08:00 a.m.
-		arrive_time = 616; // 10:16 a.m.
+		printf("8:00 a.m., arriving at 10:16 a.m.\n");
 	}
-
-	// 09:43 a.m.
-	curr_time_diff = 583 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time2 + (dep_time3 - dep_time2) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 583; //  09:43 a.m.
-		arrive_time = 712; // 11:52 a.m.
+		printf("9:43 a.m., arriving at 11:52 a.m.\n");
 	}
-
-	// 11:19 a.m.
-	curr_time_diff = 679 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time3 + (dep_time4 - dep_time3) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 679; // 11:19 a.m.
-		arrive_time = 811; // 01:31 p.m.
+		printf("11:19 a.m., arriving at 1:31 p.m.\n");
 	}
-
-	// 12:47 p.m.
-	curr_time_diff = 767 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time4 + (dep_time5 - dep_time4) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 767; // 12:47 p.m.
-		arrive_time = 900; // 03:00 p.m.
+		printf("12:47 p.m., arriving at 3:00 p.m.\n");
 	}
-
-	// 02:00 p.m.
-	curr_time_diff = 840 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time5 + (dep_time6 - dep_time5) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 840; // 02:00 p.m.
-		arrive_time = 968; // 04:08 p.m.
+		printf("2:00 p.m., arriving at 4:08 p.m.\n");
 	}
-
-	// 03:45 p.m.
-	curr_time_diff = 945 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time6 + (dep_time7 - dep_time6) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 945; // 03:45 p.m.
-		arrive_time = 1075; // 05:55 p.m.
+		printf("3:45 p.m., arriving at 5:55 p.m.\n");
 	}
-
-	// 07:00 p.m.
-	curr_time_diff = 1140 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
+	else if (total_desired_time <= (dep_time7 + (dep_time8 - dep_time7) / 2))
 	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 1140; // 07:00 p.m.
-		arrive_time = 1280; // 09:20 p.m.
-	}
-
-	// 09:45 p.m.
-	curr_time_diff = 1305 - desired_time; // +ve or -ve
-	curr_time_diff = (curr_time_diff > 0) ? curr_time_diff : -curr_time_diff; // fix sign
-
-	if(min_time_diff > curr_time_diff)
-	{
-		min_time_diff = curr_time_diff; // update new min diff
-		closest_dep_time = 1305; // 09:45 p.m.
-		arrive_time = 1438; // 11:58 p.m.
-	}
-
-	// Convert back the departure time from minutes format to hh:mm format
-	hh_dep = closest_dep_time / 60;
-	mm_dep = closest_dep_time % 60;
-
-	// Convert from 24-hr format to 12-hr format with meridiem
-	if(hh_dep == 0) // Midnight
-	{
-		hh_dep += 12;
-		dep_meridiem = 'a';
-	}
-	else if(hh_dep >= 12)
-	{
-		if(hh_dep > 12)
-			hh_dep -= 12;
-
-		dep_meridiem = 'p';
+		printf("7:00 p.m., arriving at 9:20 p.m.\n");
 	}
 	else
 	{
-		dep_meridiem = 'a';
+		printf("9:45 p.m., arriving at 11:58 p.m.\n");
 	}
 
-	// Convert back the arrival time from minutes format to hh:mm format
-	hh_arrive = arrive_time / 60;
-	mm_arrive = arrive_time % 60;
-
-	// Convert from 24-hr format to 12-hr format with meridiem
-	if(hh_arrive == 0) // Midnight
-	{
-		hh_arrive += 12;
-		arrive_meridiem = 'a';
-	}
-	else if(hh_arrive >= 12)
-	{
-		if(hh_arrive > 12)
-			hh_arrive -= 12;
-
-		arrive_meridiem = 'p';
-	}
-	else
-	{
-		arrive_meridiem = 'a';
-	}
-
-
-	printf("Closest departure time is %d:%.2d %c.m., ", hh_dep, mm_dep, dep_meridiem);
-	printf("arriving at %d:%.2d %c.m.", hh_arrive, mm_arrive, arrive_meridiem);
 
 	return 0;
 }
-
