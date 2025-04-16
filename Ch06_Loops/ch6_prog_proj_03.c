f745454f16b42ff22e34f7080fc9288c3ee4cff8
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-04-16 15:41:16 +0200

    update ch6/pp3 solution

--- Ch06_Loops/ch6_prog_proj_03.c
+++ Ch06_Loops/ch6_prog_proj_03.c
@@ -6,7 +6,7 @@
  */
 
 // Programming Project 3: Fraction's lowest terms (GCD)
-// New solution from 2024 (related to programming project 2)
+
 #include <stdio.h>
 
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 	int numerator, denominator, m, n, remainder;
 
 	printf("Enter a fracation: ");
-	scanf("%d / %d", &numerator, &denominator);
+	scanf("%d /%d", &numerator, &denominator);
 
 	m = numerator;
 	n = denominator;
@@ -30,50 +30,3 @@ int main(void)
 
 	return 0;
 }
-
-
-// Old solution from 2019
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-	int num1, den1, num2, den2, gcd = -1;
-
-	printf("Enter a fraction: ");
-	scanf("%d/%d", &num1, &den1);
-
-	if(num1 == den1)
-	{
-		printf("In lowest terms: 1/1");
-		return 0;
-	}
-
-	num2 = num1, den2 = den1;
-	while(1)
-	{
-		if(den2 == 0)
-		{
-			gcd = num2;
-			break;
-		}
-		else if(den2 == 1)
-		{
-			gcd = 1;
-			break;
-		}
-
-		gcd = num2 % den2;
-		num2 = den2;
-		den2 = gcd;
-	}
-
-	num1 /= gcd;
-	den1 /= gcd;
-
-	printf("In lowest terms: %d/%d", num1, den1);
-
-	return 0;
-}
-*/
