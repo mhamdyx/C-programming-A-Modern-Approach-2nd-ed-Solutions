3709e23645fcb655fe10a745965da0697ac1894f
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-04-28 21:38:39 +0300

    update ch9 pp2 solution

--- Ch09_Functions/ch9_prog_proj_02.c
+++ Ch09_Functions/ch9_prog_proj_02.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Enter the taxable income: ");
 	scanf("%f", &income);
 
-	printf("Tax due = %f", compute_tax(income));
+	printf("Tax due = %.2f", compute_tax(income));
 
 	return 0;
 }
@@ -27,30 +27,18 @@ float compute_tax(float income)
 {
 	float tax = 0.0f;
 
-	if(income > 0 && income <= 750)
-	{
+	if (income <= 750)
 		tax = 0.01f * income;
-	}
-	else if(income > 750 && income <= 2250)
-	{
+	else if (income <= 2250)
 		tax = 7.5f + 0.02f * income;
-	}
-	else if(income > 2250 && income <= 3750)
-	{
+	else if (income <= 3750)
 		tax = 37.50f + 0.03f * income;
-	}
-	else if(income > 3750 && income <= 5250)
-	{
+	else if (income <= 5250)
 		tax = 82.50f + 0.04f * income;
-	}
-	else if(income > 5250 && income <= 7000)
-	{
+	else if (income <= 7000)
 		tax = 142.50f + 0.05f * income;
-	}
-	else if(income > 7000)
-	{
+	else if (income > 7000)
 		tax = 230.00f + 0.06f * income;
-	}
 
 	return tax;
 }
