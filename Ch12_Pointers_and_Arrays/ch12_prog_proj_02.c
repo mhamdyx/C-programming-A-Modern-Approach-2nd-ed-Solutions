085fc5b56e3a39019208aa9f32818b4a3ac7b97f
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-05-05 20:38:35 +0300

    update ch12 pp2 solution

--- Ch12_Pointers_and_Arrays/ch12_prog_proj_02.c
+++ Ch12_Pointers_and_Arrays/ch12_prog_proj_02.c
@@ -16,56 +16,40 @@
 
 int main(void)
 {
-	char input, message[LEN] = {0};
-	int msg_len = 0, left_index, right_index;
+	char input, message[LEN];
+	int left_index = 0, right_index = 0;
 	bool palindrome = true;
 
-	printf("Enter a message: ");
-
 	// Reading the message
-	input = getchar();
+	printf("Enter a message: ");
+	while ((input = getchar()) != '\n' && right_index < LEN)
+		if (isalpha(input))
+			message[right_index++] = tolower(input);
 
-	while(input != '\n' && msg_len < LEN)
-	{
-		if((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z'))
-		{
-			message[msg_len] = (input <= 'Z') ? tolower(input) : input;
-			msg_len++;
-		}
-		input = getchar();
-	}
+	// Go back to the message end
+	right_index--;
 
 	// Palindrome Detection
-	left_index = 0;
-	right_index = msg_len - 1;
-	while(left_index <= right_index)
+	while (left_index < right_index)
 	{
-		if(message[left_index] != message[right_index])
+		if (message[left_index++] != message[right_index--])
 		{
 			palindrome = false;
 			break;
 		}
-
-		left_index++;
-		right_index--;
 	}
 
-	if(palindrome)
-	{
+	if (palindrome)
 		printf("Palindrome");
-	}
 	else
-	{
 		printf("Not a palindrome");
-	}
-
-
 
 	return 0;
 }
 */
 
 // (b)
+/*
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
@@ -73,50 +57,33 @@ int main(void)
 
 int main(void)
 {
-	char input, message[LEN] = {0}, *msg_ptr = message, *left_ch, *right_ch;
+	char input, message[LEN], *left_ch = message, *right_ch = message;
 	bool palindrome = true;
 
-	printf("Enter a message: ");
-
 	// Reading the message
-	input = getchar();
+	printf("Enter a message: ");
+	while ((input = getchar()) != '\n' && right_ch < message + LEN)
+		if (isalpha(input))
+			*right_ch++ = tolower(input);
 
-	while(input != '\n' && msg_ptr < message + LEN)
-	{
-		if((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z'))
-		{
-			*msg_ptr++ = (input <= 'Z') ? tolower(input) : input;
-		}
-		input = getchar();
-	}
-	msg_ptr--;
+	// Go back to the message end
+	right_ch--;
 
 	// Palindrome Detection
-	left_ch = message;
-	right_ch = msg_ptr;
-
-	while(left_ch <= right_ch)
+	while (left_ch < right_ch)
 	{
-		if(*left_ch != *right_ch)
+		if (*left_ch++ != *right_ch--)
 		{
 			palindrome = false;
 			break;
 		}
-
-		left_ch++;
-		right_ch--;
 	}
 
-	if(palindrome)
-	{
+	if (palindrome)
 		printf("Palindrome");
-	}
 	else
-	{
 		printf("Not a palindrome");
-	}
-
-
 
 	return 0;
 }
+*/
