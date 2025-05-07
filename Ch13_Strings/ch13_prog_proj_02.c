9bd636bc4aa87d3798ce49e3539ab1d541d52b07
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-05-08 00:02:33 +0300

    update ch13 pp2 solution

--- Ch13_Strings/ch13_prog_proj_02.c
+++ Ch13_Strings/ch13_prog_proj_02.c
@@ -19,10 +19,11 @@ int main(void)
 	char reminders[MAX_REMIND][MSG_LEN + 8];
 	char msg_str[MSG_LEN + 1], time_str[12]; // mm/dd hh:mm\0
 	int  month, day, i, j, num_remind = 0, hh, mm;
+	int month_days[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	for(;;)
+	for (;;)
 	{
-		if(num_remind == MAX_REMIND)
+		if (num_remind == MAX_REMIND)
 		{
 			printf("-- No space left --\n");
 			break;
@@ -30,14 +31,13 @@ int main(void)
 
 		printf("Enter day, 24-hour time, and reminder: ");
 		scanf("%2d/%2d", &month, &day);
-		if(day == 0 || month == 0)
-		{
+
+		if (day == 0 || month == 0)
 			break;
-		}
-		else if(day < 0 || day > 31 || month < 0 || month > 12)
+		else if (day < 0 || month < 0 || month > 12 || day > month_days[month - 1])
 		{
 			printf("Invalid reminder (enter a valid day number)\n");
-			while(getchar() != '\n');
+			while (getchar() != '\n');
 			continue;
 		}
 
@@ -46,13 +46,11 @@ int main(void)
 		sprintf(time_str, "%02d/%02d %02d:%02d", month, day, hh, mm);
 		read_line(msg_str, MSG_LEN);
 
-		for(i = 0; i < num_remind; i++)
-		{
-			if(strcmp(time_str, reminders[i]) < 0)
+		for (i = 0; i < num_remind; i++)
+			if (strcmp(time_str, reminders[i]) < 0)
 				break;
-		}
 
-		for(j = num_remind; j > i; j--)
+		for (j = num_remind; j > i; j--)
 			strcpy(reminders[j], reminders[j - 1]);
 
 		strcpy(reminders[i], time_str);
@@ -60,9 +58,9 @@ int main(void)
 		num_remind++;
 	}
 
-	printf("\nDay Reminder\n");
-	for(i = 0; i < num_remind; i++)
-		printf(" %s\n", reminders[i]);
+	printf("\nDate  Time  Reminder\n");
+	for (i = 0; i < num_remind; i++)
+		printf("%s\n", reminders[i]);
 
 	return 0;
 }
@@ -71,9 +69,10 @@ int read_line(char str[], int n)
 {
 	int ch, i = 0;
 
-	while((ch = getchar()) != '\n')
-		if(i < n)
+	while ((ch = getchar()) != '\n')
+		if (i < n)
 			str[i++] = ch;
+
 	str[i] = '\0';
 
 	return i;
