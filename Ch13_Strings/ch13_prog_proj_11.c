2bbc2250dc125181b0b8bb1b9cc7eb30830b9200
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-05-08 02:03:31 +0300

    update ch13 pp11 solution

--- Ch13_Strings/ch13_prog_proj_11.c
+++ Ch13_Strings/ch13_prog_proj_11.c
@@ -8,21 +8,21 @@
 // Programming Project 11: Average word length for a sentence
 
 #include <stdio.h>
-#define SENT_LEN 100
+#include <ctype.h>
+#define SENT_LEN 99
 
 double compute_average_word_length(char *sentence);
 void read_sentence(char *sentence, int sent_len);
 
 int main(void)
 {
-	char sentence[SENT_LEN];
+	char sentence[SENT_LEN + 1];
 	float avg;
 
 	printf("Enter a sentence: ");
 	read_sentence(sentence, SENT_LEN);
 
 	avg = compute_average_word_length(sentence);
-
 	printf("Average word length: %.1f\n", avg);
 
 	return 0;
@@ -33,35 +33,36 @@ double compute_average_word_length(char *sentence)
 	int total_len = 0, word_len = 0, word_count = 0;
 	float avg;
 
-	while(*sentence)
+	while (*sentence)
 	{
-		if(*sentence++ == ' ')
+		if (isspace(*sentence))
 		{
 			total_len += word_len;
 			word_count++;
 			word_len = 0;
+
+			while (isspace(*++sentence)); // skip more spaces between words
+			word_len++; // counting for new word
 		}
 		else
-		{
 			word_len++;
-		}
+
+		sentence++;
 	}
 
 	word_count++;
 	total_len += word_len;
-
 	avg = ((float) total_len) / word_count;
 
 	return avg;
 }
 
-
 void read_sentence(char *sentence, int sent_len)
 {
 	int count = 0;
-	while((*sentence++ = getchar()) != '\n' && count++ < sent_len);
+	while ((*sentence++ = getchar()) != '\n' && count++ < sent_len);
 	*--sentence = '\0';
 
-	if(count > sent_len)
-		while(getchar() != '\n');
+	if (count > sent_len)
+		while (getchar() != '\n');
 }
