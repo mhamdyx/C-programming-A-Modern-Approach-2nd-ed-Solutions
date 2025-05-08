0f2dbc0ae8cf0dd54a318a0b24bc39327a235634
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-05-08 06:12:41 +0300

    update ch13 pp14 solution

--- Ch13_Strings/ch13_prog_proj_14.c
+++ Ch13_Strings/ch13_prog_proj_14.c
@@ -18,42 +18,32 @@ bool are_anagrams(const char *word1, const char *word2);
 int main(void)
 {
 	char word1[WORD_LEN + 1] = {0}, word2[WORD_LEN + 1] = {0};
-
 	char input_ch, *word_ptr;
-
 
 	// Reading the first word
 	printf("Enter first word: ");
 	word_ptr = word1;
-
-
 	do
 	{
 		input_ch = getchar();
 
-		if(isalpha(input_ch))
-		{
+		if (isalpha(input_ch))
 			*word_ptr++ = tolower(input_ch);
-		}
-
-	}while(input_ch != '\n');
+	} while (input_ch != '\n');
 
 	// Reading the second word
 	printf("Enter second word: ");
 	word_ptr = word2;
-
 	do
 	{
 		input_ch = getchar();
 
-		if(isalpha(input_ch))
-		{
+		if (isalpha(input_ch))
 			*word_ptr++ = tolower(input_ch);
-		}
 
-	}while(input_ch != '\n');
+	} while(input_ch != '\n');
 
-	if(are_anagrams(word1, word2))
+	if (are_anagrams(word1, word2))
 		printf("The words are anagrams\n");
 	else
 		printf("The words are not anagrams\n");
@@ -63,23 +53,20 @@ int main(void)
 
 bool are_anagrams(const char *word1, const char *word2)
 {
-	int anagrams[26] = {0};
-	int word_len = strlen(word1);
+	int i, anagrams[26] = {0};
 
-	if(word_len != strlen(word2)) // Not the same length detection
+	while (*word1 && *word2)
+	{
+		anagrams[*word1++ - 'a']++;
+		anagrams[*word2++ - 'a']--;
+	}
+
+	if (*word1 != *word2)
 		return false;
 
-	for(int i = 0; i < word_len; i++)
-	{
-		anagrams[word1[i] - 'a']++;
-		anagrams[word2[i] - 'a']--;
-	}
-
-	for(int i = 0; i < 26; i++)
-	{
-		if(anagrams[i])
+	for (i = 0; i < 26; i++)
+		if (anagrams[i])
 			return false;
-	}
 
 	return true;
 }
