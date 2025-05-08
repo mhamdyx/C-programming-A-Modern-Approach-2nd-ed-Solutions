3ccd6c580e3aa7f68a945a6bac2d24f695ae5d00
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-05-08 17:11:06 +0300

    update ch13 pp18 solution

--- Ch13_Strings/ch13_prog_proj_18.c
+++ Ch13_Strings/ch13_prog_proj_18.c
@@ -8,21 +8,20 @@
 // Programming Project 18: Date Format
 
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
 	int dd, mm, yyyy;
 	char *months[] =
 	{
-			"January", "February", "March", "April", "May", "June",
-			"July", "August", "September", "October", "November", "December"
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"
 	};
 
 	printf("Enter a data (mm/dd/yyyy): ");
-	scanf("%d / %d / %d", &mm, &dd, &yyyy);
+	scanf("%d /%d /%d", &mm, &dd, &yyyy);
 
-	printf("You entered the data %s %d, %d", months[mm - 1], dd, yyyy);
+	printf("You entered the data %s %d, %d\n", months[mm - 1], dd, yyyy);
 
 	return 0;
 }
