0b486faf45b670f3bf4b2648b975a918b7f60129
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-06-14 23:13:45 +0300

    update ch21 pp1 solution

--- Ch21_The_Standard_Library/ch21_prog_proj_01.c
+++ Ch21_The_Standard_Library/ch21_prog_proj_01.c
@@ -21,22 +21,22 @@ int main(void)
 {
 	struct s example;
 
-	printf("Size of a = %d\n", sizeof(example.a));
-	printf("Offset of a = %d\n\n", offsetof(struct s, a));
+	printf("Size of a = %llu\n", sizeof(example.a));
+	printf("Offset of a = %llu\n\n", offsetof(struct s, a));
 
-	printf("Size of b = %d\n", sizeof(example.b));
-	printf("Offset of b = %d\n\n", offsetof(struct s, b));
+	printf("Size of b = %llu\n", sizeof(example.b));
+	printf("Offset of b = %llu\n\n", offsetof(struct s, b));
 
-	printf("Size of c = %d\n", sizeof(example.c));
-	printf("Offset of c = %d\n\n", offsetof(struct s, c));
+	printf("Size of c = %llu\n", sizeof(example.c));
+	printf("Offset of c = %llu\n\n", offsetof(struct s, c));
 
-	printf("Size of the whole structure = %d\n", sizeof(struct s));
+	printf("Size of the whole structure = %llu\n", sizeof(struct s));
 
 	return 0;
 }
 
 /*
- * From the output, we get that there is a hole of size 3 bytes lies between member a
- * and member b of the structure
- * 
+ * From the output, we get that there is a hole of size 3 bytes that lies
+ * between member a and member b of the structure
+ *
  */
