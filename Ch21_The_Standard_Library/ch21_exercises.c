09a55d75b129eca875df455209447d0ec8cef32b
mhamdyx <mahmoudhamdy.eeng@gmail.com>
2025-06-14 12:01:25 +0300

    update ch21 exercises

--- Ch21_The_Standard_Library/ch21_exercises.c
+++ Ch21_The_Standard_Library/ch21_exercises.c
@@ -11,14 +11,23 @@
  * Depending on your OS and your toolchain
  *
  * If you are using windows, probably you are using MinGW, Cygwin, ...etc.
- * Default path: C:\<your_toolchain>\lib\gcc\<target>\
+ * Default path: C:\<your_toolchain>\include
  *
- * If you are using linux, you can locate gcc header files starting with these paths:
+ * Examples:
+ * cygwin64: C:\cygwin64\usr\include
+ * MinGW: C:\MinGW\include
+ * mingw64: C:\mingw64\x86_64-w64-mingw32\include
+ *
+ * If you are using linux, you can locate gcc header files starting with
+ * this path:
  * usr/include/
- * usr/lib/gcc/
  *
+ * When you browse to any of these locations, you know which headers
+ * are standard, otherwise are non-standard and maybe platform-dependent.
  *
- * We know which headers are standard, otherwise are non-standard.
+ * Whether you are using Windows and/or Linux, there are other locations
+ * for header files, but this is dependent on one's installation of the
+ * toolchains.
  *
  */
 
@@ -27,11 +36,30 @@
 // Q2
 
 /*
- * In <stdio.h> using windows and MinGW: The function getwc is hidden by a macro
+ * The answer to this question depends on the compiler you are using,
+ * the version of the compiler, and the platform (your OS).
+ *
+ * First, try to locate where the c libraries are stored.
+ * This is totally dependent on where you installed the compiler,
+ * but if you used the default installation settings, check Q1 for ideas
+ *
+ * Second, open any header file (stdio.h as a suggestion), and then search
+ * for macros (search keyword: #define), then when you spot a function name,
+ * try to use that name as a search keyword to spot where the function
+ * declaration is, then bingo! you found one.
+ *
+ * Here is an example I used many years ago when I installed MinGW on my
+ * windows 10 machine:
+ *
+ * In <stdio.h> using windows and MinGW: 
+ * The function getwc is hidden by a macro
  *
  * Line 775: wint_t __cdecl getwc(FILE *_File);
  * Line 866: #define getwc(_stm) fgetwc(_stm)
  *
+ * Please note that the provided example may not be found for your case, so
+ * it's better to work this question out by yourself using the provided
+ * guidelines, good luck :)
  *
  */
 
@@ -40,14 +68,13 @@
 // Q3
 
 /*
- *
- * The function prototype should appear first so that the preprocessor knows
- * what function is being hidden by the macro. If the macro was to be added
- * first, macro expansions would be done later to the function declaration
- * and sytax errors arise.
+ * The function prototype must always appear first for the declaration
+ * to take effect later in the compilation process. If the macro was
+ * to be added first, macro expansions would be done later (during the
+ * pre-processing stage and before the compilation stage) to the
+ * function declaration and syntax errors would arise.
  *
  */
-
 
 //----------------------------------
 
@@ -141,11 +168,19 @@
 // Q5
 
 /*
- * Because according to the C standard, a parameterized macro that substitutes
- * for a library function must be "fully protected" by parentheses and must
- * evaluate its arguments exactly once but the given
- * macro is neither fully protected nor evaluates its argument twice which may lead
- * to problems.
+ * Because according to the C standard, a parameterized macro that
+ * substitutes for a library function must be "fully protected" by
+ * parentheses and must evaluate its arguments exactly once.
+ * While the given macro is fully protected by parentheses, it will
+ * have to evaluate its argument twice which may lead to problems.
+ *
+ * For example: Imagine the situation when we call this macro with
+ * an expression that has side effects
+ *
+ * islower(i++)
+ *
+ * This will be expanded to: ((i++) >= 'a' && (i++) <= 'z')
+ * which is an undefined behavior.
  *
  */
 
@@ -154,14 +189,17 @@
 // Q6
 
 /*
- * a) Because these types of identifiers (beginning with underscore) are reserved
- * for use only within libraries and according to the C standard, macros appearing
- * in a standard header can't be redefined if the header is included. So it's kind of
- * restriction and protection to these macros.
+ * a) Because these types of identifiers (beginning with underscore) are
+ * reserved for use only within libraries and according to the C standard,
+ * macros appearing in a standard header can't be redefined if the header
+ * is included, so it's kind of restriction and protection to these macros.
  *
  *
- * b) _ctype array will contain a bit-field of length 8 bits (unsigned char)
- * corresponding to theproperties of each character in the ASCII character set.
+ * b) _ctype array will contain a bit-field of length 8 bits (of type
+ * char or unsigned char, doesn't matter since they are ANDed with
+ * integers) corresponding to the properties of each character in
+ * the ASCII character set. Each element in the array is indexed by the
+ * ASCII value.
  *
  * Horizontal-tab character:
  * _ctype[9] = _CONTROL | _SPACE | _BLANK = 0x08 | 0x20 | 0x80
@@ -177,10 +215,14 @@
  * ^ character:
  * _ctype[94] = _PUNCT = 0x10 = 16
  *
+ * Hint: To verify this answer, create a small program that calls the
+ * ctype functions that properties of the character.
  *
  *
- * c) Advantage: Fast searching and evaluation of parameter will be done once
- * (meeting the c standard conditions)
+ *
+ * c) Advantages:
+ * 1) Performance: Fast searching as the array provides a random access
+ * 2) Parameters are evaluated once (meeting the c standard conditions)
  *
  *
  */
